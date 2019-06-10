7c0b25891ba92e34f43d98dc7291e07c437ed2e1
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-10 16:59:12 +0100

    Initial commit

---
+++ script_vis_1.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(readr)
+
+full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
+
+# Visualisation 1 ####
+full_trains %>% 
+  ggplot(aes(x = journey_time_avg)) +
+  geom_histogram(bins = 20, alpha = .4, fill = "black") +
+  labs(title = "Histogram of Average Journey Time for trains on the SNCF Network",
+       x = "Journey Time (mins.)",
+       y = "Number of Instances") +
+  theme(text = element_text(size = 15)) 
